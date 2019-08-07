dc82183aa3b2cf4cda6abbf3987df10177c07398
Andres Mena <ballerlikemike23@gmail.com>
2019-08-06 23:55:27 -0400

    Need to complete logic for functions

--- app.js
+++ app.js
@@ -3,3 +3,23 @@ function computerPlay() {
   const cpuPick = game[Math.floor(Math.random() * game.length)]
   return cpuPick
 }
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+function tieMessage() {
+
+}
+
+function winMessage() {
+
+}
+
+function loseMessage() {
+
+}
+
+function game() {
+
+}
