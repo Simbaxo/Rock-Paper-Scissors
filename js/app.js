e9b2c95a3205168246ce3a6a0135bf2a1c5892e7
Andres Mena <ballerlikemike23@gmail.com>
2019-11-11 21:02:33 -0500

    Basic Logic for game

--- js/app.js
+++ js/app.js
@@ -7,3 +7,27 @@ const scoreboard = {
   player: 0,
   computer: 0
 }
+
+// Play Game
+function play(e) {
+  restart.style.display = 'inline-block'
+  const playerChoice = e.target.id
+  const computerChoice = getComputerChoice()
+
+  console.log(playerChoice, computerChoice)
+}
+
+// Get Computers Choice
+function getComputerChoice() {
+  const rand = Math.random()
+  if (rand < 0.34) {
+    return 'rock'
+  } else if (rand <= 0.67) {
+    return 'paper'
+  } else {
+    return 'choices'
+  }
+}
+
+// Event Listeners
+choices.forEach(choice => choice.addEventListener('click', play))
