79efc954090a765ec139f3b7915a29cd6dc88454
Andres Mena <ballerlikemike23@gmail.com>
2019-11-11 21:17:21 -0500

    Display Winner Modal Logic

--- js/app.js
+++ js/app.js
@@ -14,46 +14,88 @@ function play(e) {
   const playerChoice = e.target.id
   const computerChoice = getComputerChoice()
   const winner = getWinner(playerChoice, computerChoice)
-
-  console.log(playerChoice, computerChoice, winner)
+  showWinner(winner, computerChoice)
 }
 
 // Get Computers Choice
 function getComputerChoice() {
   const rand = Math.random()
   if (rand < 0.34) {
-    return 'rock'
+    return 'Rock'
   } else if (rand <= 0.67) {
-    return 'paper'
+    return 'Paper'
   } else {
-    return 'choices'
+    return 'Choices'
   }
 }
 
 // Get Game Winner
 function getWinner(p, c) {
   if (p === c) {
-    return 'draw'
-  } else if (p === 'rock') {
-    if (c === 'paper') {
-      return 'computer'
+    return 'Draw'
+  } else if (p === 'Rock') {
+    if (c === 'Paper') {
+      return 'Computer'
     } else {
-      return 'player'
+      return 'Player'
     }
-  } else if (p === 'paper') {
-    if (c === 'scissors') {
-      return 'computer'
+  } else if (p === 'Paper') {
+    if (c === 'Scissors') {
+      return 'Computer'
     } else {
-      return 'player'
+      return 'Player'
     }
-  } else if (p === 'scissors') {
-    if (c === 'rock') {
-      return 'computer'
+  } else if (p === 'Scissors') {
+    if (c === 'Rock') {
+      return 'Computer'
     } else {
-      return 'player'
+      return 'Player'
     }
+  }
+}
+
+function showWinner(winner, computerChoice) {
+  if (winner === 'player') {
+    // Increment Player Score
+    scoreboard.player++
+    // Show Modal Result
+    result.innerHTML = `
+      <h1 class='text-win'>You Win</h1>
+      <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+      <p>Computer Chose <strong>${computerChoice}</strong></p>
+    `
+  } else if (winner === 'computer') {
+    // Increment Computer Score
+    scoreboard.computer++
+    // Show Modal Result
+    result.innerHTML = `
+      <h1 class='text-lose'>You Lose</h1>
+      <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+      <p>Computer Chose <strong>${computerChoice}</strong></p>
+    `
+  } else {
+    result.innerHTML = `
+      <h1>You Draw</h1>
+      <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+      <p>Computer Chose <strong>${computerChoice}</strong></p>
+    `
+  }
+  // Show Score
+  score.innerHTML = `
+    <p>Player: ${scoreboard.player}</p>
+    <p>Player: ${scoreboard.computer}</p>
+  `
+
+  modal.style.display = 'block'
+}
+
+// Clear Modal
+function clearModal(e) {
+  if(e.target === modal) {
+    modal.style.display = 'none'
   }
 }
 
 // Event Listeners
 choices.forEach(choice => choice.addEventListener('click', play))
+window.addEventListener('click', clearModal)
